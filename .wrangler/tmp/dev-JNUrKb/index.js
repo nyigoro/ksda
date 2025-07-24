a4fb7198e44161d562bedb94f0d4fae47b6f9b88
nyigoro <nyigoro@gmail.com>
2025-07-24 09:58:41 +0300

    commit

--- .wrangler/tmp/dev-JNUrKb/index.js
+++ .wrangler/tmp/dev-JNUrKb/index.js
@@ -90,7 +90,7 @@ var jsonError = /* @__PURE__ */ __name(async (request, env, _ctx, middlewareCtx)
 }, "jsonError");
 var middleware_miniflare3_json_error_default = jsonError;
 
-// .wrangler/tmp/bundle-YqVtyV/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-k59Tfe/middleware-insertion-facade.js
 var __INTERNAL_WRANGLER_MIDDLEWARE__ = [
   middleware_ensure_req_body_drained_default,
   middleware_miniflare3_json_error_default
@@ -122,7 +122,7 @@ function __facade_invoke__(request, env, ctx, dispatch, finalMiddleware) {
 }
 __name(__facade_invoke__, "__facade_invoke__");
 
-// .wrangler/tmp/bundle-YqVtyV/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-k59Tfe/middleware-loader.entry.ts
 var __Facade_ScheduledController__ = class ___Facade_ScheduledController__ {
   constructor(scheduledTime, cron, noRetry) {
     this.scheduledTime = scheduledTime;
